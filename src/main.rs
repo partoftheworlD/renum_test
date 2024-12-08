73759695572b3eccb29837d491324ecd68622c1e
partoftheworlD <partoftheworld@vivaldi.net>
2024-12-08 19:29:56 +0300

    some refactoring

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,25 @@ use windows::{
     Win32::System::WindowsProgramming::SYSTEM_PROCESS_INFORMATION,
 };
 
+//SYSTEM_INFORMATION_CLASS enum
+enum Sic {
+    SysProcessList = 5,
+}
+
 fn read_pwstr(pwstr: *const u16) -> String {
-    let mut len = 0;
-    while unsafe { *pwstr.offset(len) } != 0 {
+    let mut len = 0u32;
+    while unsafe { *pwstr.offset(len.try_into().unwrap()) } != 0 {
         len += 1;
     }
-    let slice = unsafe { std::slice::from_raw_parts(pwstr, len as usize) };
+    let slice = unsafe { std::slice::from_raw_parts(pwstr, len.try_into().unwrap()) };
     String::from_utf16_lossy(slice)
 }
 
 fn main() {
-    const SYSPROCESSINFO: SYSTEM_INFORMATION_CLASS = SYSTEM_INFORMATION_CLASS(0x5);
+    const SYSPROCESSINFO: SYSTEM_INFORMATION_CLASS =
+        SYSTEM_INFORMATION_CLASS(Sic::SysProcessList as i32);
     let mut buffer_size = 1024 * 1024;
-    let mut process_information = Vec::<u8>::with_capacity(buffer_size as usize);
+    let mut process_information = Vec::<u8>::with_capacity(buffer_size.try_into().unwrap());
     let _ = unsafe {
         NtQuerySystemInformation(
             SYSPROCESSINFO,
@@ -24,13 +30,13 @@ fn main() {
             &mut buffer_size,
         )
     };
-    let mut process_count = 0;
-    let mut count = 0;
+    let mut process_count = 0u32;
+    let mut count = 0u32;
     loop {
         let process = unsafe {
             *(process_information
                 .as_ptr()
-                .offset(count)
+                .offset(count.try_into().unwrap())
                 .cast::<SYSTEM_PROCESS_INFORMATION>())
         };
 
@@ -47,7 +53,7 @@ fn main() {
             break;
         }
         process_count += 1;
-        count += next as isize;
+        count += next;
     }
-    println!("Total processes: {process_count:?}")
+    println!("Total processes: {process_count:?}");
 }
