eb9579f1251f4d69711e4ff54332c363c60f2d76
partoftheworlD <partoftheworld@vivaldi.net>
2024-12-28 22:21:18 +0300

    Some bad code rewriting

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,27 @@ use windows::{
 enum Sic {
     SysProcessList = 5,
 }
+#[repr(C)]
+struct ProcessThings {
+    info: SYSTEM_PROCESS_INFORMATION,
+    name: String,
+    threads: u32,
+    handles: u32,
+    pid: u32,
+}
 
-fn read_pwstr(pwstr: *const u16) -> String {
-    let mut len = 0u32;
-    while unsafe { *pwstr.offset(len.try_into().unwrap()) } != 0 {
-        len += 1;
+fn read_pwstr(process: &SYSTEM_PROCESS_INFORMATION) -> String {
+    if process.ImageName.Buffer.is_null() {
+        return String::new();
     }
-    let slice = unsafe { std::slice::from_raw_parts(pwstr, len.try_into().unwrap()) };
+    let pwstr = process.ImageName.Buffer.0;
+    let slice = unsafe { std::slice::from_raw_parts(pwstr, process.ImageName.Buffer.len()) };
     String::from_utf16_lossy(slice)
 }
 
-fn main() {
-    const SYSPROCESSINFO: SYSTEM_INFORMATION_CLASS =
+fn get_process(process_name: &str) -> Vec<ProcessThings> {
+    let mut process_list: Vec<ProcessThings> = Vec::new();
+    static SYSPROCESSINFO: SYSTEM_INFORMATION_CLASS =
         SYSTEM_INFORMATION_CLASS(Sic::SysProcessList as i32);
     let mut buffer_size = 1024 * 1024;
     let mut process_information = Vec::<u8>::with_capacity(buffer_size.try_into().unwrap());
@@ -30,30 +39,44 @@ fn main() {
             &mut buffer_size,
         )
     };
-    let mut process_count = 0u32;
     let mut count = 0u32;
+
     loop {
-        let process = unsafe {
+        let process: SYSTEM_PROCESS_INFORMATION = unsafe {
             *(process_information
                 .as_ptr()
                 .offset(count.try_into().unwrap())
-                .cast::<SYSTEM_PROCESS_INFORMATION>())
+                .cast())
         };
-
         if !process.ImageName.Buffer.is_null() {
-            let pwstr_string = process.ImageName.Buffer;
-            let process_name = read_pwstr(pwstr_string.0);
-            println!(
-                "Process name: {process_name:?} pID: {:X?} Threads: {:?}",
-                process.UniqueProcessId.0, process.NumberOfThreads
-            );
+            let name = read_pwstr(&process);
+            if name.to_ascii_lowercase() == process_name {
+                process_list.push(ProcessThings {
+                    info: process,
+                    name,
+                    threads: process.NumberOfThreads,
+                    handles: process.HandleCount,
+                    pid: process.UniqueProcessId.0 as u32,
+                });
+            }
         }
+
         let next = process.NextEntryOffset;
         if next == 0 {
             break;
         }
-        process_count += 1;
         count += next;
     }
-    println!("Total processes: {process_count:?}");
+    process_list
+}
+
+fn main() {
+    let plist = get_process("gta5.exe");
+    println!("Total: {:?}", plist.len());
+    for process in plist {
+        println!(
+            "Process ID: 0x{:04X} Name: {} Threads: {:04} Handles: {:04}",
+            process.pid, process.name, process.threads, process.handles
+        );
+    }
 }
